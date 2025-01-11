8f3785656f69c5ba94a8fded614af8f8a4f56880
Ankit Gupta <iambittuankitbkg@gmail.com>
2025-01-11 10:14:57 +0530

    toggle theme bugs fixed.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const icon = document.querySelector(".fa-moon");
 const body = document.querySelector("body");
-const hero = document.querySelector("hero bg");
+const tempDiv = document.querySelector(".bg");
 const navItems = document.querySelectorAll(".nav-items ul a");
 const para = document.querySelectorAll("p");
 const company = document.querySelectorAll(".company li");
 
+console.log(tempDiv);
+
 icon.addEventListener("click", (e) => {
-  console.log(e);
   if (body.classList.contains("bg-black")) {
     navItems.forEach((item) => (item.style.color = "#000"));
     para.forEach((item) => (item.style.color = "#444343c1"));
+    company.forEach((comp) => {
+      comp.classList.remove("bg-company");
+    });
+    tempDiv.style.display = "block";
   } else {
     navItems.forEach((item) => (item.style.color = "#fff"));
     para.forEach((item) => (item.style.color = "#fffc"));
+    company.forEach((comp) => {
+      comp.classList.add("bg-company");
+    });
+    tempDiv.style.display = "none";
   }
   body.classList.toggle("bg-black");
-  hero.classList.toggle("bg");
-  company.forEach((comp) => comp.classList.toggle("bg-company"));
 });
