7d539b8e02dff1af62ea5849d17555dae9bd5f16
Ankit Gupta <iambittuankitbkg@gmail.com>
2025-01-11 21:02:04 +0530

    Animation part done

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const navItems = document.querySelectorAll(".nav-items ul a");
 const para = document.querySelectorAll("p");
 const company = document.querySelectorAll(".company li");
 
-console.log(tempDiv);
-
 icon.addEventListener("click", (e) => {
   if (body.classList.contains("bg-black")) {
     navItems.forEach((item) => (item.style.color = "#000"));
@@ -24,4 +22,120 @@ icon.addEventListener("click", (e) => {
     tempDiv.style.display = "none";
   }
   body.classList.toggle("bg-black");
+});
+
+//gsap
+
+//home
+
+gsap.from(".logo", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(".btn", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(".fa-moon", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(navItems, {
+  y: -20,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.3,
+});
+
+gsap.from(".company li img", {
+  x: -1800,
+  rotation: -360,
+  duration: 1.5,
+});
+
+gsap.from(".hero-header .right", {
+  x: 1800,
+  duration: 1.5,
+});
+
+gsap.from(".hero-footer h3, .hero-footer p", {
+  x: 1800,
+  duration: 1.5,
+});
+
+gsap.from(".hero-header .left", {
+  x: -1800,
+  duration: 1.5,
+});
+
+gsap.from(".bg", {
+  rotate: "10deg",
+});
+
+// community
+
+gsap.from(".community-content .heading", {
+  opacity: 0,
+  scale: 0.5,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: ".community-content .heading",
+    scroller: "body",
+    start: "top 70%",
+  },
+});
+
+gsap.from(".company-container .cmp-des", {
+  x: 2000,
+  duration: 1,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: ".community-content .cmp-des",
+    scroller: "body",
+    start: "top 50%",
+  },
+});
+
+//about
+
+gsap.from(".about .left", {
+  x: -1800,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: ".about .left",
+    scroller: "body",
+    start: "top 90%",
+  },
+});
+
+gsap.from(".about .right", {
+  x: 1800,
+  duration: 1.5,
+  scrollTrigger: {
+    trigger: ".about .left",
+    scroller: "body",
+    start: "top 90%",
+  },
+});
+
+//contact
+
+gsap.from(".contact .bg", {
+  minWidth: "0%",
+  height: "0",
+  duration: 2,
+  scrollTrigger: {
+    markers: true,
+    trigger: ".contact",
+    scroller: "body",
+    start: "top 100%",
+    end: "top 0%",
+    scrub: 2,
+  },
 });
